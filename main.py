43e3f22afecc80cf650f211f964e08f52e98a022
Vinzzztty <kevinarnandes21@gmail.com>
2025-07-25 07:27:19 +0700

    make: to allow for all

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Or ["*"] for all origins (not recommended for production)
+    allow_origins=["*"],  # Or ["*"] for all origins (not recommended for production)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
